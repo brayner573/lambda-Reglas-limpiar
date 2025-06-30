4483f7fa151643b23716c7a6ea1122c39883fe7d
olger856 <infaccallo@gmail.com>
2025-06-29 19:01:44 -0500

    cambio 3

--- main.tf
+++ main.tf
@@ -36,7 +36,6 @@ resource "aws_s3_bucket_notification" "lambda_trigger" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "uploads/"
     filter_suffix       = ".csv"
   }
 
